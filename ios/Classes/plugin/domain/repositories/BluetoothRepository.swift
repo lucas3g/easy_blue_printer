5c4422e555b5f735294d715873d68318bbdbd7f7
Lucas Silva <lucassilva080397@gmail.com>
2025-02-12 12:13:04 -0800

    fix: plugin ios

--- ios/Classes/plugin/domain/repositories/BluetoothRepository.swift
+++ ios/Classes/plugin/domain/repositories/BluetoothRepository.swift
@@ -2,4 +2,8 @@ import Foundation
 
 public protocol BluetoothRepository {
     func scanDevices() -> [BluetoothDeviceEntity]
+    func connectToDevice(address: String) -> Bool
+    func disconnectToDevice() -> Bool
+    func printData(data: String, size: Int, align: Int, bold: Bool) -> Bool
+    func printEmptyLine(callTimes: Int) -> Bool
 }
